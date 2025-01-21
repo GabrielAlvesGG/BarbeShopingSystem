a724f6a353ec307887406b3dd8be5ea87b960c4d
Gabriel Alves <alves.gabriel7@gmail.com>
2025-01-21 17:55:57 -0300

    Pagination has been added to the barber listing.

--- obj/Debug/net9.0/BarberShopSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/BarberShopSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+802900c39821f3614a1f072dabd287d734bd7c94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0093be7ed5a3af1bff0ed482d42b12df8a543fe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
