0093be7ed5a3af1bff0ed482d42b12df8a543fe4
Gabriel Alves <alves.gabriel7@gmail.com>
2025-01-21 17:45:13 -0300

    A new component has been added to the project using Vuetify to display for the client list.

--- obj/Debug/net9.0/BarberShopSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/BarberShopSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+674a318e725f453d473aea4764db57ab19b7b904")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+802900c39821f3614a1f072dabd287d734bd7c94")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
