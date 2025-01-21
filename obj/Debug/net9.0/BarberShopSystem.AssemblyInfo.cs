802900c39821f3614a1f072dabd287d734bd7c94
Gabriel Alves <alves.gabriel7@gmail.com>
2025-01-21 13:06:29 -0300

    Vue.js's v-cloak was insert into the system so that it does not show the raw code during page loading and some adjustments were made to the time at which the loading animation was removed from the listing screens.

--- obj/Debug/net9.0/BarberShopSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/BarberShopSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c9fa79f95521688d79f90fc80b41c0de0bd0dd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+674a318e725f453d473aea4764db57ab19b7b904")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
