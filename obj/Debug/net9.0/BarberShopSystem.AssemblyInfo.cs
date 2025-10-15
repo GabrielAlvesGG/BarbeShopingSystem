0a7d1826e2afe19db5a50433d7cdc7bd404792c1
Gabriel Alves <alves.gabriel7@gmail.com>
2025-10-15 20:12:04 -0300

    Ajuste no erro do modal.

--- obj/Debug/net9.0/BarberShopSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/BarberShopSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20ee00fc4834a7a006144acbf6ef77713474a5e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2347389c3d59800ad10470b9773616661ec3fd23")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
