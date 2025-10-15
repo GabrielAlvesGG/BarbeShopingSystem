7c552f170219ca43dee49169a7cc4d0fed6e3850
Gabriel Alves <alves.gabriel7@gmail.com>
2025-10-14 21:37:49 -0300

    Ajuste no layout e melhoria nas visualizações do projeto.Padronizando o layout.

--- obj/Debug/net9.0/BarberShopSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/BarberShopSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a75f7c708579da02b5a5db20022ffd4e70f5ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+539eec06530344164c2a73c10eaeaec3d5e0dafa")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
