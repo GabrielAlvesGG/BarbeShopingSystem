1b857edf344a83de107186d80f2a680d51d7f345
Gabriel Alves <alves.gabriel7@gmail.com>
2025-10-16 17:35:08 -0300

    Ajuste na cor do fechar do modal agendamento.

--- obj/Debug/net9.0/BarberShopSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/BarberShopSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2347389c3d59800ad10470b9773616661ec3fd23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a7d1826e2afe19db5a50433d7cdc7bd404792c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarberShopSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
